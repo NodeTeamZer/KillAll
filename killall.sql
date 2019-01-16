85db994c4df1f74ca577b3ce52e6db6ac04c9027
Jeremy Coley <jeremy.coley@tactfactory.com>
2019-01-16 23:14:36 +0100

    Updated database

--- killall.sql
+++ killall.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 16, 2019 at 03:30 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Host: localhost:8889
+-- Generation Time: Jan 16, 2019 at 10:13 PM
+-- Server version: 5.6.38
+-- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -32,8 +32,18 @@ CREATE TABLE `character` (
   `attack` int(11) NOT NULL,
   `defense` int(11) NOT NULL,
   `agility` int(11) NOT NULL,
-  `hp` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `hp` int(11) NOT NULL,
+  `kill_number` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `character`
+--
+
+INSERT INTO `character` (`id`, `nickname`, `attack`, `defense`, `agility`, `hp`, `kill_number`) VALUES
+(2, 'test2', 7, 2, 6, 40, 0),
+(3, 'aaaa', 100, 100, 100, 100, 0),
+(4, 'aaaa', 100, 100, 100, 100, 0);
 
 --
 -- Indexes for dumped tables
@@ -53,7 +63,8 @@ ALTER TABLE `character`
 -- AUTO_INCREMENT for table `character`
 --
 ALTER TABLE `character`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
