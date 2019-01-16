d6906252ea23b35e2886542cc519347028339597
Jeremy Coley <jeremy.coley@tactfactory.com>
2019-01-16 15:19:40 +0100

    init commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+const ent = require('ent');
+const http = require('http').Server(app);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'killall'
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/killAll.html');
+});
+
+io.sockets.on('connection', function (socket, pseudo) {
+    socket.on('NewPlayer', function(pseudo) {
+            pseudo = ent.encode(pseudo);
+            console.log('pseudo: '+pseudo);
+            socket.pseudo = pseudo;
+            socket.broadcast.emit('NewPlayer', pseudo);
+        });
+});
+
+
+
+/*io.sockets.on('NewPlayer', function (socket, pseudo) {
+    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    socket.on('Newplayer', function(pseudo) {
+        pseudo = ent.encode(pseudo);
+        socket.pseudo = pseudo;
+    });
+
+    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    socket.on('message', function (message) {
+        message = ent.encode(message);
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+    });
+});*/
