7194f4ca946220daeb4a4e0a80691fbcef791bcc
Jeremy Coley <jeremy.coley@tactfactory.com>
2019-01-17 13:19:04 +0100

    Finished POO refacto

---
+++ MySQLManager.js
@@ -0,0 +1,110 @@
+const mysql = require('mysql');
+
+/**
+ * String method definition used to copy the str.format() method from python.
+ */
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        const args = arguments;
+
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return (typeof args[number] != 'undefined') ? args[number] : match;
+        });
+    };
+}
+
+/**
+ * Mother class of database managers, used to manage entities into databases.
+ */
+class MySQLManager {
+    /**
+     * MySQLManager's constructor initializing the attributes.
+     */
+    constructor() {
+        this.connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : 'root',
+            port: 8889,
+            database : 'killall'
+        });
+
+        this.query = "";
+        this.NULL = "NULL";
+        this.table ="";
+        this.fields = [];
+    }
+
+    /**
+     * From a request / response, checks the parameters given and read the results from the database.
+     * @param req The request to the server.
+     * @param res The response from the server.
+     */
+    readAPI(req, res) {
+        const id = this.connection.escape(req.query.id);
+
+        if (id === this.NULL) {
+            this.query = "SELECT * FROM `{0}`".format(this.table);
+        } else {
+            this.query = "SELECT * FROM `{0}` WHERE {1} = {2}".format(this.table, this.fields[0], id);
+        }
+
+        this.connection.query(this.query, function (error, results) {
+            if (error) {
+                console.log(error);
+
+                return;
+            }
+
+            res.json({result: results});
+        });
+    }
+
+    /**
+     * From a request / response, checks the parameters given and update the associated entity into the database.
+     * @param req The request to the server.
+     * @param res The response from the server.
+     */
+    updateAPI(req, res) {
+        const id = this.connection.escape(req.query.id);
+
+        if (id === this.NULL) {
+            res.json({error: "The id should be defined."});
+        } else {
+            this.query = "UPDATE `" + this.table + "` SET ";
+            let cpt = 1;
+
+            for (let key in req.query) {
+                if (key !== "id") {
+                    this.query += key + " = " + this.connection.escape(req.query[key] + " ");
+
+                    if (cpt < (Object.keys(req.query).length)) {
+                        this.query += ", "
+                    }
+                }
+
+                cpt++;
+            }
+
+            this.query += " WHERE " + this.fields[0] + " = " + id;
+
+            this.connection.query(this.query, function (error, results) {
+                let jsonResult;
+
+                if (error) {
+                    console.log(error);
+
+                    jsonResult = {error: "An error occurred with the entity update. See console for further information."};
+                } else if (results.affectedRows === 0) {
+                    jsonResult = {error: "The entity with the id {0} doesn't exists, nothing changed.".format(id)};
+                } else {
+                    jsonResult = {result: "Entity successfully updated."};
+                }
+
+                res.json(jsonResult);
+            });
+        }
+    }
+}
+
+module.exports = MySQLManager;
---
+++ UserManager.js
@@ -0,0 +1,144 @@
+const MySQLManager = require("./MySQLManager.js");
+
+class UserManager extends MySQLManager {
+    /**
+     * UserManager's constructor, calling super and initializing default database table value.
+     */
+    constructor() {
+        super();
+
+        /**
+         * Represents the user table name.
+         * @type {string}
+         */
+        this.table = "user";
+
+        /**
+         * Stores the this.fields from the user table.
+         * @type {string[]}
+         */
+        this.fields = ["id", "login", "password"];
+
+        /**
+         * Stores the query results.
+         * @type {null}
+         */
+        this.results = null;
+    }
+
+    /**
+     * From a request, checks the parameters given and create a new user entity into the database.
+     * @param req The request to the server.
+     * @param res The response from the server.
+     */
+    createAPI(req, res) {
+        const login = this.connection.escape(req.query.login);
+        const password = this.connection.escape(req.query.password);
+
+        if (login === this.NULL || password === this.NULL) {
+            res.json({error: "Missing arguments : {0} and {1} should be set.".format(this.fields[1], this.fields[2])});
+        } else {
+            this.query = "INSERT INTO `{0}`({1}, {2}) VALUES({3}, {4})"
+                .format(this.table,
+                    this.fields[1],
+                    this.fields[2],
+                    login,
+                    password);
+
+            this.connection.query(this.query, function (error) {
+                if (error) {
+                    console.log(error);
+
+                    return;
+                }
+
+                res.json({result: "User successfully inserted."});
+            });
+        }
+    }
+
+    /**
+     * Creates a user into the database.
+     * @param login The request to the server.
+     * @param password The response from the server.
+     */
+    create(login, password) {
+        if (login === this.NULL || password === this.NULL) {
+            console.log("Missing arguments : {0} and {1} should be set.".format(this.fields[1], this.fields[2]));
+        } else {
+            this.query = "INSERT INTO `{0}`({1}, {2}) VALUES({3}, {4})"
+                .format(this.table,
+                    this.fields[1],
+                    this.fields[2],
+                    login,
+                    password);
+
+            this.connection.query(this.query, function (error) {
+                if (error) {
+                    console.log(error);
+
+                    return;
+                }
+
+                console.log("User successfully inserted.");
+            });
+        }
+    }
+
+    /**
+     * Loads a user from the database depending on the login an password given.
+     * @param login The user's login.
+     * @param password The password associated to the login.
+     * @param callback The callback function.
+     */
+    load(login, password, callback) {
+        if (login === this.NULL || password === this.NULL) {
+            return;
+        } else {
+            this.query = "SELECT * FROM `{0}` WHERE {1} = {2} AND {3} = {4}"
+                .format(this.table,
+                    this.fields[1],
+                    this.connection.escape(login),
+                    this.fields[2],
+                    this.connection.escape(password));
+        }
+
+        this.connection.query(this.query, function (error, results) {
+            if (error) {
+                console.log(error);
+
+                callback(results);
+            }
+        });
+    }
+
+    /**
+     * Used to authenticate the user when connecting.
+     * @param login The user login.
+     * @param password The user password.
+     * @param callback The callback function.
+     */
+    authenticate(login, password, callback) {
+        if (login === this.NULL || password === this.NULL) {
+            console.log("Missing arguments : {0} and {1} should be set.".format(this.fields[1], this.fields[2]));
+        } else {
+            this.query = "SELECT {0} FROM `{1}` WHERE {2} = {3} AND {4} = {5}"
+                .format(this.fields[0],
+                    this.table,
+                    this.fields[1],
+                    this.connection.escape(login),
+                    this.fields[2],
+                    this.connection.escape(password));
+
+            this.connection.query(this.query, function (error, results) {
+                if (error) {
+                    console.log(error);
+                }
+
+                callback(results[0].id);
+            });
+        }
+    }
+}
+
+module.exports = UserManager;
